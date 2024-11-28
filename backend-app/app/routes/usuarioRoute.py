84da65beab01e2135b5e00aa3fdbe80ee3293eae
Kalline Corrêa <kallinecorrea@hotmail.com>
2024-11-27 21:17:05 -0300

    adicionando rota de lisatr usuários

--- backend-app/app/routes/usuarioRoute.py
+++ backend-app/app/routes/usuarioRoute.py
@@ -9,8 +9,8 @@ from marshmallow import ValidationError
 from ..database import db
 
 @usuarioNs.route('/')
-class UsuarioPostResource(Resource):
-    @usuarioNs.doc(description='Endpoint para fazer operações do usuário')
+class UsuarioResource(Resource):
+    @usuarioNs.doc(description='Endpoint para cadastrar usuários', security='Bearer')
     @usuarioNs.expect(UsuarioModel)
     @usuarioNs.response(200, 'Successo', UsuarioModel)
     @jwt_required()
@@ -35,3 +35,12 @@ class UsuarioPostResource(Resource):
         db.session.add(novo_usuario)
         db.session.commit()
         return {"messagem": "Usuário criado com sucesso!", "usuario_id": str(novo_usuario.id)}, 201
+
+    @usuarioNs.doc(description='Endpoint para listar todos os usuários', security='Bearer')
+    @usuarioNs.response(200, 'Successo', UsuarioModel)
+    @jwt_required()
+    def get(self):
+        """Lista todos os usuários"""
+        usuarios = Usuario.query.all()
+        resultado = [{'id': str(usuario.id), 'nome': usuario.nome, 'email': usuario.email} for usuario in usuarios]
+        return {"usuarios": resultado}, 200
