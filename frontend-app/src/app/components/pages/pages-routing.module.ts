6cac0d79b255aa5dcf4ff6c7c8f9833e9bbfeb3c
Kalline Corrêa <kallinecorrea@hotmail.com>
2024-12-10 10:17:28 -0300

    ajustando rota de usuário

--- frontend-app/src/app/components/pages/pages-routing.module.ts
+++ frontend-app/src/app/components/pages/pages-routing.module.ts
@@ -4,7 +4,9 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [RouterModule.forChild([
         { path: 'denuncia', loadChildren: () => import('./denuncia/denuncia.module').then(m => m.CrudModule) },
-        { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }
+        { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+        { path: 'usuario', loadChildren: () => import('./usuario/usuario.module').then(m => m.UsuarioModule) },
+
     ])],
     exports: [RouterModule]
 })
